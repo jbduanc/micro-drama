01dc19f9e04c8c8d53b64bfefc26a526458706e9
jbduanc <djboc@outlook.com>
2026-04-24 10:34:01 +0800

    micro droma init

--- backend/micro-drama-admin/src/main/java/com/series/admin/AdminApplication.java
+++ backend/micro-drama-admin/src/main/java/com/series/admin/AdminApplication.java
@@ -3,8 +3,11 @@ package com.series.admin;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 @SpringBootApplication(scanBasePackages = "com.series")
 @MapperScan("com.series.**.mapper")
+@EnableDiscoveryClient
 public class AdminApplication {
 
     public static void main(String[] args) {
