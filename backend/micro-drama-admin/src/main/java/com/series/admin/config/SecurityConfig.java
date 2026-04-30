70b6bb81282f584ebb0d0da66176f56ac7bddb1c
jbduanc <djboc@outlook.com>
2026-04-30 09:55:50 +0800

    修改google oauth配置

--- backend/micro-drama-admin/src/main/java/com/series/admin/config/SecurityConfig.java
+++ backend/micro-drama-admin/src/main/java/com/series/admin/config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 // 权限配置
                 .authorizeRequests()
                 // 登录接口放行
-                .antMatchers("/oauth2/login/google", "/oauth2/authorize-url").permitAll()
+                .antMatchers("/admin-api/oauth2/login/google", "/admin-api/oauth2/authorize-url").permitAll()
                 // 其他所有接口 → 必须登录
                 .anyRequest().authenticated();
 
